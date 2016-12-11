8e90ad321c460acfb83db08783d0a80af5c630fb
Lucas Bovolini <lbovolini94@gmail.com>
2016-12-11 18:02:03 -0200

    Add param

--- Trabalho 8 - Planilha Eletronica/src/main.c
+++ Trabalho 8 - Planilha Eletronica/src/main.c
@@ -6,24 +6,25 @@ int main()
     matriz_esparsa *matriz1 = NULL,
                    *matriz2 = NULL;
 
-    tipo_descritor_lista *lista = cria_descritor_lista();
+    tipo_descritor_lista *expressao = cria_descritor_lista();
+    tipo_descritor_lista *operacao = cria_descritor_lista();
 
-    ler_operacoes(lista);
+    ler_operacoes(expressao, operacao);
 
     ler_arquivo(&matriz1, &matriz2);
     //ler_operacoes(matriz1);
 
     //printf("Matriz 1\n");
-    // imprime_matriz(matriz1);
+    //imprime_matriz(matriz1);
 
     //escrever_arquivo(matriz1, NULL, NULL, NULL);
 
 
     //imprime_lista(lista);
     //printf("\n");
-    calcula(lista, matriz1);
+    //calcula(lista, matriz1);
     //printf("--------------\n");
-    imprime_lista(lista);
+    //imprime_lista(lista);
 
     return 0;
 }
